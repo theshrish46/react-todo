ac724bf38168e91630bc13dd4ed053ddf57cd3c8
theshrish46 <theshrish46@gmail.com>
2023-05-26 16:41:55 +0530

    added all the components

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import { AiOutlineDelete } from 'react-icons/ai'
 
-const List = ({note}) => {
+const List = ({ title, desc }) => {
     return (
-        <div>
-            <h1 className='my-6 text-center text-cyan-500 text-3xl font-semibold'>Today's Checklist</h1>
-            <ul>
-                <li className='w-11/12 mx-auto flex justify-around items-center border-2 shadow-sm transition-all duration-700'>
-                    <div className='flex flex-col justify-center items-start'>
-                        <h4 className='my-2 text-2xl text-cyan-800 font-semibold'>Coding</h4>
-                        <p className='my-2 text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa recusandae</p>
-                    </div>
-                    <button className='h-10 w-20'><AiOutlineDelete size={30}/></button>
-                </li>
-            </ul>
+        <div className='w-11/12 mx-auto md:w-[760px] md:mx-auto lg:mx-auto lg:w-full'>
+            <li className='w-auto h-auto mx-auto my-8 p-4 flex justify-around items-center shadow-xl rounded-2xl transition-all duration-700 bg-cyan-400'>
+                <input type='checkbox' className='w-5 h-5 checked:rounded-full'/>
+                <div className='w-10/12 flex flex-col justify-between items-start'>
+                    <h4 className='my-2 text-3xl text-cyan-800 font-semibold md:text-4xl'>{title}</h4>
+                    <p className='my-2 block max-w-lg overflow-x-hidden text-2xl md:text-3xl'>{desc}</p>
+                </div>
+                <button className='h-8 w-8 hover:text-red-500'><AiOutlineDelete size={40} /></button>
+            </li>
         </div>
     )
 }
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -9,29 +9,37 @@ const Todo = () => {
 
     const submitHandler = (e) => {
         e.preventDefault()
-        console.log('clicked');
-        console.log(e.target);
+
+
+        setNote([...note, {
+            title,
+            desc
+        }])
     }
 
     return (
-        <div className='w-11/12 my-4 mx-auto shadow-lg'>
-            <h1 className='my-6 text-center text-5xl text-cyan-500 font-medium'>Todo App</h1>
-            <h3 className='my-3 text-center text-2xl font-semibold'>Track Your Day</h3>
+        <>
 
-            <form action="/" className='w-11/12 mx-auto my-5 flex flex-col justify-around items-start' onSubmit={submitHandler}>
-                <input type="text" name='title' placeholder='Work' className='w-full my-3 border-2 border-black p-3 rounded-md outline-none text-3xl'/>
-                <input type="text" name='desc' placeholder='Description' className='w-full my-3 border-2 border-black p-3 rounded-md outline-none text-3xl'/>
-                <input type='submit' className='my-3 py-3 px-6 bg-cyan-500 rounded-md text-white text-xl font-semibold'/>
-            </form>
+            <div className='w-11/12 my-4 mx-auto py-2 shadow-xl rounded-3xl bg-cyan-300 md:w-[760px] md:mx-auto lg:w-[800px] lg:mx-auto'>
+                <h1 className='my-6 text-center text-5xl text-black font-medium'>Todo App</h1>
+                <h3 className='my-3 text-center text-2xl font-semibold'>Track Your Day</h3>
+
+                <form className='w-11/12 mx-auto my-5 flex flex-col justify-around items-start' onSubmit={submitHandler}>
+                    <input type="text" name='title' placeholder='Work' className='w-full my-3 border-2 border-black p-3 rounded-md outline-none text-3xl' onChange={(e) => { setTitle(e.target.value) }} />
+                    <input type="text" name='desc' placeholder='Description' className='w-full my-3 border-2 border-black p-3 rounded-md outline-none text-3xl' onChange={(e) => { setDesc(e.target.value) }} />
+                    <input type='submit' className='my-6 py-3 px-6 bg-cyan-500 rounded-md text-white text-2xl font-semibold' />
+                </form>
+            </div>
+            <h1 className='my-12 text-center text-black text-4xl font-semibold transition-all duration-1000'>Today's Checklist</h1>
 
             {
-                note.map(() => {
-                    return(
-                        <List note={note}/>
-                    )
-                })
+                <ul className=' w-11/12 mx-auto lg:grid lg:grid-cols-2 lg:gap-10 2xl:w-4/5 2xl:mx-auto'>
+                    {note.map((item, index) => (
+                        <List key={index} title={item.title} desc={item.desc} />
+                    ))}
+                </ul>
             }
-        </div>
+        </>
     )
 }
 
